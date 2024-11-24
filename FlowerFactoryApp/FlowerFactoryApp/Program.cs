e206d648a3f21315845b2d2384e383484ef7a2b9
Danilo2077 <danilogerasymovsky1@gmail.com>
2024-11-24 09:46:51 +0200

    Add files via upload

---
+++ FlowerFactoryApp/FlowerFactoryApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FlowerFactoryLibrary1;
+
+class Program
+{
+    static void Main()
+    {
+        // Створення фабрик
+        FlowerFactory indoorFactory = new IndoorFlowerFactory();
+        FlowerFactory gardenFactory = new GardenFlowerFactory();
+        FlowerFactory wildFactory = new WildFlowerFactory();
+
+        // Створення квітів за допомогою фабрик
+        Flower indoorFlower = indoorFactory.CreateFlower();
+        Flower gardenFlower = gardenFactory.CreateFlower();
+        Flower wildFlower = wildFactory.CreateFlower();
+
+        // Виведення типу квіток
+        indoorFlower.ShowType();
+        gardenFlower.ShowType();
+        wildFlower.ShowType();
+    }
+}
